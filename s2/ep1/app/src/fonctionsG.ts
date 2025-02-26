92317c556a4976d87b9df8bb05e089b1dac01d49
Tim Hrdy <tim.hrdy.1@gmail.com>
2025-02-26 12:23:53 +0100

    e

--- s2/ep1/app/src/fonctionsG.ts
+++ s2/ep1/app/src/fonctionsG.ts
@@ -1,12 +1,9 @@
-function premierElement<T>(tableau: [T, ...T[]]): T {
-    if (tableau.length === 0) {
-        throw new Error("Le tableau ne doit pas être vide");
-    }
+function premierElement<T>(tableau: T[]): T {
+
     return tableau[0];
 }
 
-const tab1: [number, ...number[]] = [1, 2, 3];
-const tab2: [string, ...string[]] = ["a", "b", "c"];
-
-console.log(premierElement(tab1)); // Output: 1
-console.log(premierElement(tab2)); // Output: a
+const tab1 = [1, 2, 3];
+const tab2 = ["a", "b", "c"];
+console.log(premierElement(tab1));
+console.log(premierElement(tab2));
--- s2/ep2/app/src/fonctionsG.ts
+++ s2/ep2/app/src/fonctionsG.ts
@@ -1,9 +1,12 @@
-function premierElement<T>(tableau: T[]): T {
-
+function premierElement<T>(tableau: [T, ...T[]]): T {
+    if (tableau.length === 0) {
+        throw new Error("Le tableau ne doit pas être vide");
+    }
     return tableau[0];
 }
 
-const tab1 = [1, 2, 3];
-const tab2 = ["a", "b", "c"];
-console.log(premierElement(tab1));
-console.log(premierElement(tab2));
+const tab1: [number, ...number[]] = [1, 2, 3];
+const tab2: [string, ...string[]] = ["a", "b", "c"];
+
+console.log(premierElement(tab1)); // Output: 1
+console.log(premierElement(tab2)); // Output: a
